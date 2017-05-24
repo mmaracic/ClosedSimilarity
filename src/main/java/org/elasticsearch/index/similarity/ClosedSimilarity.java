2e826cb6f484bdfb00b14ab65659fecbfbe72a41
Marijo Maračić <marijo.maracic@gmail.com>
2017-05-24 18:40:03 +0200

    Alpha score function implementation.

--- src/main/java/org/elasticsearch/index/similarity/ClosedSimilarity.java
+++ src/main/java/org/elasticsearch/index/similarity/ClosedSimilarity.java
@@ -7,11 +7,17 @@ package org.elasticsearch.index.similarity;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.logging.Level;
 import org.apache.log4j.Logger;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.index.Fields;
+import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.search.CollectionStatistics;
 import org.apache.lucene.search.TermStatistics;
 import org.apache.lucene.search.similarities.Similarity;
@@ -28,7 +34,7 @@ public class ClosedSimilarity extends Similarity{
     public ClosedSimilarity(){}
 
 
-    /** Cache of decoded bytes. */
+    /** list of fields/attributes and their weights */
     private static final Map<String, Long> attribWeights = new HashMap<>();
 
     static {
@@ -76,90 +82,246 @@ public class ClosedSimilarity extends Similarity{
         return normValue;
     }
 
-    /** Implemented as <code>log(1 + (numDocs - docFreq + 0.5)/(docFreq + 0.5))</code>. */
+    /** Implemented as <code>log(1 + (numDocs - docFreq + 0.5)/(docFreq + 0.5))</code>.
+     * @param docFreq
+     * @param numDocs
+     * @return  
+     */
     protected float idf(long docFreq, long numDocs) {
       return (float) Math.log(1 + (numDocs - docFreq + 0.5D)/(docFreq + 0.5D));
     }
     
     @Override
     public SimWeight computeWeight(CollectionStatistics collectionStats, TermStatistics... termStats) {
-        float idf = 0.0f;
-        String desc="Field: "+collectionStats.field()+" Terms: ";
+        Map<String, QueryTokenInfo> termIdfs = new HashMap<>();
+        String desc="Target field: "+collectionStats.field()+" Terms: ";
         final long max = collectionStats.maxDoc();
-        for (final TermStatistics stat : termStats ) {
+        for (final TermStatistics stat : termStats) {
             final long df = stat.docFreq();
             final float termIdf = idf(df, max);
-            idf += termIdf;
-            log.info("Calculating term frequency: "+stat.term().utf8ToString()+" Value: "+df);
+            QueryTokenInfo qti = new QueryTokenInfo(stat.term(), collectionStats.field(),df,termIdf);
+            termIdfs.put(stat.term().utf8ToString(), qti);
+            log.info("Calculating term: "+stat.term().utf8ToString()+" frequency: "+df+" Appearance: "+df);
             desc += stat.term().utf8ToString() + " ";
         }
-        log.info("Calculating term idf: "+desc+" Value: "+idf);
-        ClosedSimWeight csw = new ClosedSimWeight(collectionStats.field(), idf, termStats);
-        log.info("Calculating sim weight for field: "+csw.desc);
+        log.info("Calculating sim weight for field: "+desc);
+        ClosedSimWeight csw = new ClosedSimWeight(collectionStats.field(), desc, termIdfs);
         return csw;
     }
 
     @Override
     public SimScorer simScorer(SimWeight weight, LeafReaderContext context) throws IOException {
         ClosedSimWeight csWeight = (ClosedSimWeight) weight;
-        ClosedSimScorer css = new ClosedSimScorer(csWeight, context.reader().getNormValues(csWeight.field));
+        ClosedSimScorer css = new ClosedSimScorer(csWeight, context);
         log.info("Returning sim scorer for: "+csWeight.field);
         return css;
+    }
+    
+    private class QueryTokenInfo{
+        private final BytesRef rawToken;
+        private final String attribute;
+        private final long count;
+        private final float idf;
+        
+        public QueryTokenInfo(BytesRef rawToken, String attribute, long count, float idf){
+            this.rawToken = rawToken;
+            this.attribute = attribute;
+            this.count = count;
+            this.idf = idf;
+        }
+
+        public BytesRef getRawToken() {
+            return rawToken;
+        }
+        public String getAttribute() {
+            return attribute;
+        }
+        public long getCount() {
+            return count;
+        }
+        public float getIdf() {
+            return idf;
+        }
     }
     
     private static class ClosedSimWeight extends SimWeight{
         
         private String field;
-        /** idf */
-        private final float idf;
-        /** query boost */
-        private float boost;
-        /** weight (idf * boost) */
-        private float weight;
         
-        private String desc="";
+        private Map<String, QueryTokenInfo> termInfos;
         
-        ClosedSimWeight(String field, float idf, TermStatistics... termStats){
-            this.idf = idf;
+        private String desc;
+        
+        ClosedSimWeight(String field, String description, Map<String, QueryTokenInfo> termInfos){
+            this.termInfos = termInfos;
             this.field = field;
-            
-            desc="Field: "+field+" Terms: ";
-            for (final TermStatistics stat : termStats ) {
-                desc += stat.term().utf8ToString()+" ";
-            }
-            log.info("Creating weight for : "+desc);
+            this.desc = description;
         }
 
         @Override
         public float getValueForNormalization() {
-            float wNorm = weight * weight;
-            log.info("Returning weight normalization. Sim: "+desc+" Value: "+wNorm);
-            return wNorm;
+            //float wNorm = weight * weight;
+            log.info("Returning weight normalization. Sim: "+desc+" Value: "+1f);
+            return 1f;
         }
 
         @Override
         public void normalize(float queryNorm, float boost) {
-            this.boost = boost;
-            this.weight = idf * boost;
-            log.info("Calculating weight normalization. Sim: "+desc+" qNorm: "+queryNorm+" boost: "+boost+" weight: "+weight);
+            log.info("Calculating weight normalization. Sim: "+desc+" qNorm: "+queryNorm+" boost: "+boost);
        }    
     }
     
     private class ClosedSimScorer extends SimScorer {
         
-        private final float weightValue; // boost * idf * (k1 + 1)
         private final ClosedSimWeight csw;
+        
+        private final LeafReaderContext context;
 
-        ClosedSimScorer(ClosedSimWeight weights, NumericDocValues norms){
+        ClosedSimScorer(ClosedSimWeight weights, LeafReaderContext context){
             log.info("Creating scorer: "+weights.desc);
             this.csw = weights;
-            this.weightValue = csw.weight;
+            this.context = context;
         }
+        
+        /**
+         * Estimate term weight according to term field frequency
+         * @param docFields
+         * @param termIdfs
+         * @return 
+         */
+        private float estimateTermWeight(LeafReader indexReader, BytesRef rawTerm){
+            log.info("Estimating term weight: "+rawTerm.utf8ToString());
+            try{
+                Map<String, Long> fieldFrequency = new HashMap<>();
+                long totalTermFreq = 0;
+                for(String field: attribWeights.keySet()){
+                    Term term = new Term(field, rawTerm);
+                    long termFreq = indexReader.totalTermFreq(term);
+                    if (termFreq>0){
+                        fieldFrequency.put(field, termFreq);
+                        log.info("Term: "+rawTerm.utf8ToString()+" frequency: "+rawTerm.utf8ToString()+" in field: "+field);
+                        totalTermFreq+=termFreq;
+                    } else {
+                        log.info("Term: "+rawTerm.utf8ToString()+" frequency is 0 in field: "+field);
+                    }
+                }
+                log.info("Term: "+rawTerm.utf8ToString()+" total frequency: "+totalTermFreq);
+                float weight = 0;
+                for(String field: attribWeights.keySet()){
+                    long fieldWeight = attribWeights.get(field);
+                    if (fieldFrequency.containsKey(field)){
+                        weight += (fieldFrequency.get(field)/(float)totalTermFreq)*fieldWeight;
+                    }
+                }
+                log.info("Term: "+rawTerm.utf8ToString()+" WEIGHT: "+weight);
+                return weight;
+            } catch(IOException ex){
+                
+            }
+            return 1f;
+        }
+        
+        /**
+         * Estimates norm for an index based on attributes/fields it contains
+         * 
+         * @param indexReader index reader
+         * @return norm
+         */
+        private float estimateIndexNorm(LeafReader indexReader){
+            try {
+                log.info("Estimating index norm.");
+                float norm = 0f;
+                Fields fs = indexReader.fields();
+                Iterator<String> it = fs.iterator();
+                while(it.hasNext()){
+                    String fName = it.next();
+                    if (attribWeights.containsKey(fName)){
+                        long pow = attribWeights.get(fName);
+                        norm += Math.pow(2d, pow);
+                    }
+                }
+                log.info("Index norm: "+norm);
+                return norm;
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(ClosedSimilarity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return 0f;
+        }
+        
+        /**
+         * Estimate doc coordination toward query
+         * @param docFields
+         * @param termIdfs
+         * @return 
+         */
+        private float docCoord(Document doc, Map<String, QueryTokenInfo> termInfos){
+            log.info("Estimating doc coordination. for doc id: "+doc);
+            Map<String, Long> docTokenFreq = new HashMap<>();
+            long docTokenCount = 0;
+            for(String field: attribWeights.keySet()){
+                String[] tokens = doc.getValues(field);
+                for(String token: tokens){
+                    docTokenCount++;
+                    if (docTokenFreq.containsKey(token)){
+                        long freq = docTokenFreq.get(token);
+                        docTokenFreq.put(token, freq+1l);
+                    } else {
+                        log.info("Document's field: "+field+" has token: "+token);
+                        docTokenFreq.put(token, 1l);
+                    }
+                }
+            }
+            
+            long missing = 0;
+            for (String docToken: docTokenFreq.keySet()){
+                if (!termInfos.containsKey(docToken)){
+                    log.info("Document's token missing: "+docToken);
+                    missing+= docTokenFreq.get(docToken);
+                } else {
+                    long docCount = docTokenFreq.get(docToken);
+                    long queryCount = termInfos.get(docToken).getCount();
+                    long diff = (docCount > queryCount)?docCount - queryCount:0;
+                    missing += diff;
+                }
+            }
+            log.info("Document's tokens missing in query: "+missing);
+            float docCoord = (float)(docTokenCount - missing)/docTokenCount;
+            log.info("Document's coordination: "+docCoord);
+            return docCoord;
+        }
+        
+        
                 
         @Override
         public float score(int doc, float freq) {
-            log.info("Computing score. Scorer: "+csw.desc+" Score: "+weightValue);
-            return weightValue;
+            try {
+                log.info("ID of the document: "+doc);
+                Document document = context.reader().document(doc);
+                float docCoord = docCoord(document, csw.termInfos);
+
+                float score = 0;
+                float indexNorm = estimateIndexNorm(context.reader());
+                float termNorm = context.reader().getNormValues(csw.field).get(doc);
+                for(String term: csw.termInfos.keySet()){
+                    QueryTokenInfo qti = csw.termInfos.get(term);
+                    if (qti.getAttribute().compareTo("_all")==0){
+                        termNorm = estimateTermWeight(context.reader(), qti.getRawToken());
+                    }
+                    float idfGain =(9f + (2f * qti.getIdf()))/10f;
+                    log.info("Idf gain for term "+term+" is: "+idfGain);
+                    float sumPart = (termNorm/indexNorm) * idfGain;
+                    log.info("Sum part for term "+term+" is: "+sumPart);
+                    score+= sumPart;
+                }
+                log.info("Scorer: "+csw.desc+" Score after summation: "+score);
+                score /= csw.termInfos.keySet().size();
+                score *= docCoord;
+                log.info("Scorer: "+csw.desc+" Score without query: "+score);
+                return score;
+            } catch (IOException ex) {
+                log.error(ex);
+            }
+            return 0f;
         }
 
         @Override
